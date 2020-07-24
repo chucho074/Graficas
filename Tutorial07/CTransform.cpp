9ca722d6a1e44f80c1661c619c5936a95d3801a4
Jesus Alberto del Moral Cupil <idv18c.jmoral@uartesdigitales.edu.mx>
2020-07-23 19:22:20 -0500

    Update

--- Tutorial07/CTransform.cpp
+++ Tutorial07/CTransform.cpp
@@ -15,32 +15,33 @@ CTransform::~CTransform() {
 
 mat4 CTransform::getMatrix() {
 	mat4 TranMatrix (1.f);
-	//Multiply by rot in X
-	TranMatrix[1][1] = TranMatrix[1][1] * cos(m_Rot.x);
-	TranMatrix[2][1] = TranMatrix[2][1] * -sin(m_Rot.x);
-	TranMatrix[1][2] = TranMatrix[1][2] * sin(m_Rot.x);
-	TranMatrix[1][1] = TranMatrix[1][1] * cos(m_Rot.x);
-	//Multiply by rot in Y
-	TranMatrix[0][0] = TranMatrix[0][0] * cos(m_Rot.y);
-	TranMatrix[2][0] = TranMatrix[2][0] * sin(m_Rot.y);
-	TranMatrix[0][2] = TranMatrix[0][2] * -sin(m_Rot.y);
-	TranMatrix[2][2] = TranMatrix[2][2] * cos(m_Rot.y);
-	//Multiply by rot in Z
-	TranMatrix[0][0] = TranMatrix[0][0] * cos(m_Rot.z);
-	TranMatrix[1][0] = TranMatrix[1][0] * -sin(m_Rot.z);
-	TranMatrix[0][1] = TranMatrix[0][1] * sin(m_Rot.z);
-	TranMatrix[1][1] = TranMatrix[1][1] * cos(m_Rot.z);
-
-	//Multiply by Scale
-	TranMatrix[0][0] = TranMatrix[0][0] * m_Scale.x;
-	TranMatrix[1][1] = TranMatrix[1][1] * m_Scale.y;
-	TranMatrix[2][2] = TranMatrix[2][2] * m_Scale.z;
-	//Multiply by Traslation
-	TranMatrix[3][0] = TranMatrix[3][0] * m_Tras.x;
-	TranMatrix[3][1] = TranMatrix[3][2] * m_Tras.y;
-	TranMatrix[3][2] = TranMatrix[3][2] * m_Tras.z;
-
-
+	////Multiply by rot in X
+	//TranMatrix[1][1] = TranMatrix[1][1] * cos(m_Rot.x);
+	//TranMatrix[2][1] = TranMatrix[2][1] * -sin(m_Rot.x);
+	//TranMatrix[1][2] = TranMatrix[1][2] * sin(m_Rot.x);
+	//TranMatrix[1][1] = TranMatrix[1][1] * cos(m_Rot.x);
+	////Multiply by rot in Y
+	//TranMatrix[0][0] = TranMatrix[0][0] * cos(m_Rot.y);
+	//TranMatrix[2][0] = TranMatrix[2][0] * sin(m_Rot.y);
+	//TranMatrix[0][2] = TranMatrix[0][2] * -sin(m_Rot.y);
+	//TranMatrix[2][2] = TranMatrix[2][2] * cos(m_Rot.y);
+	////Multiply by rot in Z
+	//TranMatrix[0][0] = TranMatrix[0][0] * cos(m_Rot.z);
+	//TranMatrix[1][0] = TranMatrix[1][0] * -sin(m_Rot.z);
+	//TranMatrix[0][1] = TranMatrix[0][1] * sin(m_Rot.z);
+	//TranMatrix[1][1] = TranMatrix[1][1] * cos(m_Rot.z);
+	//
+	////Multiply by Scale
+	//TranMatrix[0][0] = TranMatrix[0][0] * m_Scale.x;
+	//TranMatrix[1][1] = TranMatrix[1][1] * m_Scale.y;
+	//TranMatrix[2][2] = TranMatrix[2][2] * m_Scale.z;
+	////Multiply by Traslation
+	//TranMatrix[3][0] = TranMatrix[3][0] * m_Tras.x;
+	//TranMatrix[3][1] = TranMatrix[3][2] * m_Tras.y;
+	//TranMatrix[3][2] = TranMatrix[3][2] * m_Tras.z;
+	rotate(TranMatrix, 0.f ,m_Rot);
+	scale(TranMatrix, m_Scale);
+	translate(TranMatrix, m_Tras);
 
 	return TranMatrix;
 }
